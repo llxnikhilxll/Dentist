3448adb7cc65e74aad80c7064e6e9da212c1a136
llxnikhilxll <146409161+llxnikhilxll@users.noreply.github.com>
2024-07-09 07:56:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HomePage from './components/LandingPage/HeroPage'
+import Page2 from './components/Page2/Page2'
+
+const App = () => {
+  return (
+    <div>
+      <HomePage />
+      <Page2 />
+    </div>
+  )
+}
+
+export default App
